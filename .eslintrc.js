a1c18fe1f2fd007b263020f09a0652df207076af
soojang <solitasroh@gmail.com>
2022-04-07 22:10:59 +0900

    코딩 룰 적용관련 파일 커밋

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'eslint-config-prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    // airbnb ESLint 구성의 문제를 해결하기 위함
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      // eslint-plugin-react에게 사용하고 있는 리액트의 버전을 알아서 탐지하도록 한다.
+      version: 'detect',
+    },
+    // 해당 확장자 모듈 가져오도록 등록
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { BrowserRouter,Route, Link } from "react-router-dom";
-import MainRouter from "./Routes/MainRouter";
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import MainRouter from './Routes/MainRouter';
 
 function App() {
-  return <div className="App"><MainRouter /></div>;
+  return (
+    <div className="App">
+      <MainRouter />
+    </div>
+  );
 }
 
 export default App;

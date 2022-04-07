b22fb34e3c385ae780a36f1f6899d2ee633c9dbf
nahyoungkim <82586783+nahyoungkim@users.noreply.github.com>
2022-04-07 22:02:29 +0900

    Login 화면 초기 커밋

---
+++ src/Components/Login/LoginButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+
+interface Props {
+    onClick?: () => void;
+    image?: string;
+}
+const Container = styled.div`
+    display: flex;
+`;
+const ButtonContainer = styled.button<Props>`
+  border: transparent 0;
+  background: transparent;
+  outline: none;
+  cursor : pointer;
+`;
+export default function LoginButton({ onClick, image }: Props): React.ReactElement {
+    return (
+        <Container>
+            <ButtonContainer onClick={onClick}>
+            <img alt="test" src={image}/>
+            </ButtonContainer>
+        </Container>
+    )
+}

e8202ed0f611b07051802e3512b2752477acb029
nahyoungkim <82586783+nahyoungkim@users.noreply.github.com>
2022-04-13 13:51:21 +0900

    init 화면 추가

--- src/Pages/Game/InitPage.tsx
+++ src/Pages/Game/InitPage.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import { Button, Input } from 'antd';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import startImage from '../../Assets/startButton3.png';
+import startChangeImage from '../../Assets/startButton1.png';
 
 interface Props {
   id: number;
 }
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 400px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background: #979797;
+`;
+const TitleContainer = styled.div`
+  display: flex;
+  width: 500px;
+  height: 100px;
+  left: 223px;
+  top: 186px;
+  align-items: center;
+  justify-content: center;
+  background: #f07a7a;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 77px;
+  color: #ffffff;
+`;
+const StartButton = styled.div`
+  display: flex;
+  top: 100px;
+  cursor: pointer;
+`;
+const InputContainer = styled.div`
+  display: flex;
+`;
 function InitPage({ id }: Props): React.ReactElement {
-  return <div>InitPage</div>;
+  const [over, setOver] = useState(false);
+  return (
+    <>
+      <Container>
+        <TitleContainer>방구석 놀토</TitleContainer>
+        <StartButton onMouseOver={() => setOver(!over)}>
+          <img alt="test" src={over ? startChangeImage : startImage} />
+        </StartButton>
+      </Container>
+      <InputContainer>
+        <Input />
+        <Button type="primary">submit</Button>
+      </InputContainer>
+    </>
+  );
 }
 
 export default InitPage;
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { useNavigate } from 'react-router-dom';
 import buttonImage from '../Assets/kakao_login_medium_narrow.png';
 import googleImage from '../Assets/btn_google_signin_dark_normal_web.png';
 import LoginButton from '../Components/Login/LoginButton';
@@ -16,9 +17,13 @@ const Container = styled.div`
 `;
 
 function Login() {
+  const navigate = useNavigate();
+  
   const onClick = (): void => {
     console.log('button click!');
+    navigate(`/Game/Init`);
   };
+
   return (
     <Container>
       <LoginButton onClick={onClick} image={buttonImage} />

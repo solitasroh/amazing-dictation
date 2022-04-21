7bddb722c33ab7cc6a4ebb7bc990878d4e966dcf
nahyoungkim <82586783+nahyoungkim@users.noreply.github.com>
2022-04-21 20:38:00 +0900

    노래 Intro 페이지와 Play 페이지 추가

--- src/Pages/Game/PlayPage.tsx
+++ src/Pages/Game/PlayPage.tsx
@@ -1,10 +1,120 @@
-import React from 'react';
+import { Button, Input } from 'antd';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import Countdown from 'react-countdown';
 
 interface Props {
   id: number;
 }
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 400px;
+  flex-direction: column;
+  align-items: center;
+  background: #979797;
+`;
+const IyricsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 550px;
+`;
+const ShowIyrics = styled.div`
+  font-weight: 200;
+  font-size: 20px;
+  line-height: 50px;
+  color: #ffffff;
+`;
+const SecretIyrics = styled.div`
+  display: flex;
+`;
+const SecretWords = styled.div`
+  display: flex;
+  font-size: 25px;
+  width: 30px;
+  height: 30px;
+  margin: 2px;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ffe600;
+  color: #ffffff;
+`;
+const HeadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const CounterAnimation = keyframes`
+ from {   
+  transform: translatex(0);
+  }
+  to { 
+    transform: translateX(-10px);
+  }
+`;
+const CounterContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  font-size: medium;
+  font-weight: 500;
+  color: #970c0c;
+  animation: ${CounterAnimation} 10 0.5s;
+`;
+const TitleContainer = styled.div`
+  display: flex;
+  width: 150px;
+  height: 50px;
+  margin-bottom: 50px;
+  align-items: center;
+  justify-content: center;
+  font-weight: 200;
+  font-size: 30px;
+  line-height: 77px;
+  color: #ffffff;
+`;
+const InputContainer = styled.div`
+  display: flex;
+`;
 function PlayPage({ id }: Props): React.ReactElement {
-  return <div>PlayPage</div>;
+  const secretSong = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  ];
+  const [isShow, setShow] = useState(true);
+  return (
+    <>
+      <Container>
+        {isShow ? (
+          <>
+            <CounterContainer>
+              {' '}
+              제한시간: <Countdown date={Date.now() + 10000} />
+            </CounterContainer>
+            <TitleContainer>노래 제목</TitleContainer>
+            <IyricsContainer>
+              <ShowIyrics>
+                넌 역시 Trouble! Trouble! Trouble! 때를 노렸어 너는 Shoot!
+                Shoot! Shoot! 나는 훗! 훗! 훗!
+              </ShowIyrics>
+              <SecretIyrics>
+                {secretSong.map(index => (
+                  <SecretWords key={index}>{index}</SecretWords>
+                ))}
+              </SecretIyrics>
+              <ShowIyrics>
+                넌 역시 Trouble! Trouble! Trouble! 때를 노렸어 너는 Shoot!
+                Shoot! Shoot! 나는 훗! 훗! 훗!
+              </ShowIyrics>
+            </IyricsContainer>
+          </>
+        ) : null}
+      </Container>
+      <InputContainer>
+        <Input />
+        <Button type="primary">submit</Button>
+      </InputContainer>
+    </>
+  );
 }
 
 export default PlayPage;
--- src/Pages/Game/SongIntroPage.tsx
+++ src/Pages/Game/SongIntroPage.tsx
@@ -1,10 +1,36 @@
 import { Button, Input } from 'antd';
-import React from 'react';
-import styled from 'styled-components';
+import React, { useRef, useState } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import { useNavigate } from 'react-router-dom';
+import styled, { css, keyframes } from 'styled-components';
 
 interface Props {
   id: number;
 }
+interface TimeProps {
+  isTimeUp?: boolean;
+  isTimeDown?: boolean;
+}
+const boxAnimation = keyframes`
+ from {   
+  opacity: 0;
+    transform: translateY(-100%);
+  }
+  to { 
+  transform: translateY(0);
+  opacity: 1;
+  }
+`;
+const boxAnimation2 = keyframes`
+ from {   
+  transform: translateY(0);
+  opacity: 1;
+  }
+  to { 
+    opacity: 0;
+    transform: translateY(100%);
+  }
+`;
 const Container = styled.div`
   display: flex;
   width: 100%;
@@ -13,6 +39,38 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-around;
   background: #979797;
+`;
+const TimerContainer = styled.div`
+  position: relative;
+  width: 80px;
+  height: 60px;
+  font-size: 48px;
+  font-family: 'Montserrat';
+`;
+
+const TimerChange = styled.div<TimeProps>`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+  animation: ${boxAnimation} 3 1s;
+`;
+const TimerChange1 = styled.div<TimeProps>`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  animation: ${boxAnimation2} 3 1s;
 `;
 const TitleContainer = styled.div`
   display: flex;
@@ -30,14 +88,66 @@ const TitleContainer = styled.div`
   line-height: 77px;
   color: #ffffff;
 `;
+
 const InputContainer = styled.div`
   display: flex;
 `;
+
+function RenderTime({
+  remainingTime,
+}: {
+  remainingTime: number;
+}): React.ReactElement {
+  const currentTime = useRef(remainingTime);
+  const prevTime = useRef(0);
+  const isNewTimeFirstTick = useRef(false);
+  const [, setOneLastRerender] = useState(0);
+
+  if (currentTime.current !== remainingTime) {
+    isNewTimeFirstTick.current = true;
+    prevTime.current = currentTime.current;
+    currentTime.current = remainingTime;
+  } else {
+    isNewTimeFirstTick.current = false;
+  }
+
+  // force one last re-render when the time is over to tirgger the last animation
+  if (remainingTime === 0) {
+    setTimeout(() => {
+      setOneLastRerender((val: number) => val + 1);
+    }, 20);
+  }
+
+  const isTimeUp = isNewTimeFirstTick.current;
+  return (
+    <TimerContainer>
+      <TimerChange>{remainingTime}</TimerChange>
+      {prevTime.current !== 0 && (
+        <TimerChange1>{prevTime.current}</TimerChange1>
+      )}
+    </TimerContainer>
+  );
+}
+
 function SongIntroPage({ id }: Props): React.ReactElement {
+  const navigate = useNavigate();
+
+  const onComplete = (): void => {
+    navigate(`/Game/Play`);
+  };
   return (
     <>
       <Container>
         <TitleContainer>노래 제목</TitleContainer>
+        <CountdownCircleTimer
+          isPlaying
+          duration={3}
+          colors={['#004777', '#F7B801', '#A30000']}
+          colorsTime={[3, 2, 0]}
+          onComplete={onComplete}
+        >
+          {RenderTime}
+        </CountdownCircleTimer>
       </Container>
       <InputContainer>
         <Input />

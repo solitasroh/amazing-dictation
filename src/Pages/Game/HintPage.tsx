31e127799653689638e803161616201b31a2c09b
nahyoungkim <82586783+nahyoungkim@users.noreply.github.com>
2022-05-03 16:01:53 +0900

    결과 페이지 및 힌트 페이지 추가

--- src/Pages/Game/HintPage.tsx
+++ src/Pages/Game/HintPage.tsx
@@ -1,10 +1,58 @@
+import { ThunderboltOutlined, ThunderboltTwoTone } from '@ant-design/icons';
+import { Button, Input } from 'antd';
 import React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   id: number;
 }
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 400px;
+  flex-direction: column;
+  align-items: center;
+  background: #979797;
+`;
+const HintContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+const HintBox = styled.div`
+  display: flex;
+  width: 150px;
+  height: 150px;
+  border: 1px solid white;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  font-weight: 400;
+  font-size: 20px;
+`;
 function HintPage({ id }: Props): React.ReactElement {
-  return <div>HintPage</div>;
+  return (
+    <Container>
+      <HintContainer>
+        <HintBox>전체 띄어쓰기</HintBox>
+        <HintBox>한글자</HintBox>
+        <HintBox>초성</HintBox>
+      </HintContainer>
+      <HintContainer>
+        <HintBox>70% 듣기</HintBox>
+        <HintBox>
+          5분의 1초
+          <ThunderboltTwoTone
+            twoToneColor="#ffd900"
+            style={{ fontSize: '40px' }}
+          />
+        </HintBox>
+      </HintContainer>
+    </Container>
+  );
 }
 
 export default HintPage;
--- src/Pages/Game/ResultPage.tsx
+++ src/Pages/Game/ResultPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Button } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface Props {
@@ -10,6 +11,7 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   height: 400px;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background: #979797;
@@ -23,14 +25,46 @@ const TitleContainer = styled.div`
   font-size: 128px;
   line-height: 129px;
 `;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 60px;
+  line-height: 129px;
+`;
+
 function ResultPage({ id }: Props): React.ReactElement {
   const { state } = useLocation();
-  console.log(state);
+  const navigate = useNavigate();
   const result = state ? '정답' : '실패';
+  const [nextStage, setNextStage] = useState('');
+
+  useEffect(() => {
+    if (result === '실패') {
+      setNextStage('힌트를 볼까요?');
+    } else {
+      setNextStage('다음 게임으로?');
+    }
+  }, []);
+  const onClick = () => {
+    if (result === '실패') {
+      navigate(`/Game/Hint`);
+    } else {
+      navigate(`/Game/Loading`);
+    }
+  };
   return (
-    <Container>
-      <TitleContainer>{result}</TitleContainer>
-    </Container>
+    <>
+      <Container>
+        <TitleContainer>{result}</TitleContainer>
+      </Container>
+      <ButtonContainer>
+        <Button type="primary" onClick={onClick}>
+          {nextStage}
+        </Button>
+      </ButtonContainer>
+    </>
   );
 }
 

0ce3692eec1eda926b28f8aea283955c3b0b0ab1
solitasroh <78634894+solitasroh@users.noreply.github.com>
2022-05-10 21:07:02 +0900

    login 화면 디자인 수정 style-reset 적용

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -5,30 +5,50 @@ import { useNavigate } from 'react-router-dom';
 import buttonImage from '../Assets/kakao_login_medium_narrow.png';
 import googleImage from '../Assets/btn_google_signin_dark_normal_web.png';
 import LoginButton from '../Components/Login/LoginButton';
+import LogoImg from '../Assets/Logo.png';
 
-const Container = styled.div`
-  display: flex;
-  width: 100%;
+const MainContainer = styled.div`
+  position: absolute;
+  flex-direction: row;
   height: 100vh;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: #ffffff;
+  width: 100%;
+  align-content: flex-end;
+  align-items: flex-end;
+  user-select: none;
+`;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  display: flex;
+  margin: 20px;
+`;
+const LogoContainer = styled.div`
+  position: relative;
+`;
+const Logo = styled.img`
+  width: fit-content;
+  height: fit-content;
+  margin: 20px;
 `;
 
 function Login() {
   const navigate = useNavigate();
-  
+
   const onClick = (): void => {
     console.log('button click!');
     navigate(`/Game/Init`);
   };
 
   return (
-    <Container>
-      <LoginButton onClick={onClick} image={buttonImage} />
-      <LoginButton onClick ={onClick} image ={googleImage} />
-    </Container>
+    <MainContainer>
+      <LogoContainer>
+        <Logo src={LogoImg} />
+      </LogoContainer>
+      <ButtonContainer>
+        <LoginButton onClick={onClick} image={buttonImage} />
+        <LoginButton onClick={onClick} image={googleImage} />
+      </ButtonContainer>
+    </MainContainer>
   );
 }
 
